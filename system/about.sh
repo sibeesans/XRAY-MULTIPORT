904dc8023e8bf58e4c1b569ed19b35e58feae91f
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-05-26 19:07:53 +0700

    Update about.sh

--- system/about.sh
+++ system/about.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #wget https://github.com/${GitUser}/
-GitUser="PelangiSenja"
+GitUser="Kulanbagong1"
 #IZIN SCRIPT
 MYIP=$(curl -sS ipv4.icanhazip.com)
 echo -e "\e[32mloading...\e[0m"
@@ -8,7 +8,7 @@ clear
 # Valid Script
 VALIDITY () {
     today=`date -d "0 days" +"%Y-%m-%d"`
-    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
+    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/izinn/main/ipvps.conf | grep $MYIP | awk '{print $4}')
     if [[ $today < $Exp1 ]]; then
     echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
     else
@@ -17,7 +17,7 @@ VALIDITY () {
     exit 0
 fi
 }
-IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
+IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/izinn/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
 if [ $MYIP = $IZIN ]; then
 echo -e "\e[32mPermission Accepted...\e[0m"
 VALIDITY
@@ -38,11 +38,11 @@ gl='\e[32;1m'
 clear
 echo ""
 figlet " SCRIPT" | lolcat
-figlet " BY pelangisenja" | lolcat
+figlet " BY JengkolOnline" | lolcat
 echo -e "\e[32;1m.-----------------------------------------------.\e[0m"
 echo -e "\e[32;1m|               \e[36;1mINFO SCRIPT VPS\e[0m                 \e[32;1m|\e[0m"
 echo -e "\e[32;1m'-----------------------------------------------'\e[0m"
-echo -e "           \e[031;1m> Premium Script By PelangiSenja <\e[0m" | lolcat
+echo -e "           \e[031;1m> Premium Script By JengkolOnline <\e[0m" | lolcat
 echo -e " \e[0;32m_______________________________________________\e[0m"
 echo -e "\e[0;32m|    \e[1;35mFor OS Debian 10 & Ubuntu 20.04 64 bit     \e[0;32m|\e[0m"
 echo -e "\e[0;32m|  \e[1;35mFor VPS with KVM and VMWare PelangiSenja         \e[0;32m|\e[0m"
