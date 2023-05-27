d990bce5dbfe2afd5ebc3b2040f705529979d6fe
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-05-27 17:28:36 +0700

    Add files via upload

---
+++ wireguard/menuwg.sh
@@ -0,0 +1,77 @@
+#wget https://github.com/${GitUser}/
+GitUser="Kulanbagong1"
+#IZIN SCRIPT
+MYIP=$(curl -sS ipv4.icanhazip.com)
+echo -e "\e[32mloading...\e[0m"
+clear
+# Valid Script
+VALIDITY () {
+    today=`date -d "0 days" +"%Y-%m-%d"`
+    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/izinn/main/ipvps.conf | grep $MYIP | awk '{print $4}')
+    if [[ $today < $Exp1 ]]; then
+    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
+    else
+    echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m";
+    echo -e "\e[31mPlease renew your ipvps first\e[0m"
+    exit 0
+fi
+}
+IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/izinn/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo -e "\e[32mPermission Accepted...\e[0m"
+VALIDITY
+else
+echo -e "\e[31mPermission Denied!\e[0m";
+echo -e "\e[31mPlease buy script first\e[0m"
+exit 0
+fi
+echo -e "\e[32mloading...\e[0m"
+clear
+# LINE COLOUR
+line=$(cat /etc/line)
+# TEXT COLOUR BELOW
+below=$(cat /etc/below)
+# BACKGROUND TEXT COLOUR
+back_text=$(cat /etc/back)
+# NUMBER COLOUR
+number=$(cat /etc/number)
+# TEXT ON BOX COLOUR
+box=$(cat /etc/box)
+clear
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- ifconfig.me/ip);
+clear
+echo -e ""
+echo -e "   \e[$line════════════════════════════════════════\e[m"
+echo -e "   \e[$back_text             \e[30m═[\e[$box MENU Wireguard\e[30m ]═          \e[m"
+echo -e "   \e[$line════════════════════════════════════════\e[m"
+echo -e "   \e[$number (•1)\e[m \e[$below Buat Akun Wireguard\e[m"
+echo -e "   \e[$number (•2)\e[m \e[$below Delet Akun Wireguard\e[m"
+echo -e "   \e[$number (•3)\e[m \e[$below Renew Akun Wireguard\e[m"
+echo -e "   \e[$line════════════════════════════════════════\e[m"
+echo -e "   \e[$back_text \e[$box x)   MENU                             \e[m"
+echo -e "   \e[$line════════════════════════════════════════\e[m"
+echo -e "\e[$line"
+read -p "    Please Input Number  [1-17 or x] :  "  sys
+echo -e ""
+case $sys in
+1)
+addwg
+;;
+2)
+delwg
+;;
+3)
+renewwg
+;;
+x)
+menu
+;;
+*)
+echo "Please enter an correct number"
+sleep 1
+system
+;;
+esac
