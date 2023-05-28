087e9b576db7ba9bdbb5cc520d519cbf3fc5dafd
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-05-29 00:52:11 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -152,7 +152,7 @@ clear
 #install Xray
 echo -e "\e[0;32mINSTALLING XRAY CORE...\e[0m"
 sleep 1
-wget https://raw.githubusercontent.com/${GitUser}/XRAY-MULTIPORT/main/install/ins-xray.sh && chmod +x ins-xray.sh && screen -S ins-xray ./ins-xray.sh
+wget https://raw.githubusercontent.com/${GitUser}/XRAY-MULTIPORT/main/install/ins-xray2.sh && chmod +x ins-xray2.sh && screen -S ins-xray2 ./ins-xray2.sh
 echo -e "\e[0;32mDONE INSTALLING XRAY CORE\e[0m"
 clear
 #install ohp-server
