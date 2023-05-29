4c28b339cfe8effe0d79218404164b6ab5741868
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-05-29 18:57:42 +0700

    Update ins-xray3.sh

--- install/ins-xray3.sh
+++ install/ins-xray3.sh
@@ -147,9 +147,9 @@ apt install -y nginx
 cd
 rm -fr /etc/nginx/sites-enabled/default
 rm -fr /etc/nginx/sites-available/default
-wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/PelangiSenja/Blueblue/main/nginx.conf" 
+wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/nginx.conf" 
 #mkdir -p /home/vps/public_html
-wget -q -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/PelangiSenja/Blueblue/main/vps.conf"
+wget -q -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/vps.conf"
 
 # Install Xray #
 #==========#
