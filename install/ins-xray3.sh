6dd7e3fe0ffec1dfba34682993b752d74cbfa011
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-05-30 10:56:15 +0700

    Update ins-xray3.sh

--- install/ins-xray3.sh
+++ install/ins-xray3.sh
@@ -781,3 +781,121 @@ echo -e "[ ${GREEN}ok${NC} ] Restart & Xray & Nginx"
 systemctl daemon-reload >/dev/null 2>&1
 systemctl restart xray >/dev/null 2>&1
 systemctl restart nginx >/dev/null 2>&1
+
+cd /usr/bin
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Main Menu${NC}"
+wget -q -O vmess "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/menu/vmess.sh"
+wget -q -O vless "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/menu/vless.sh"
+wget -q -O trojan "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/menu/trojan.sh"
+wget -q -O shadowsocks "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/menu/shadowsocks.sh"
+wget -q -O shadowsocks2022 "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/menu/shadowsocks2022.sh"
+wget -q -O socks "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/menu/socks.sh"
+wget -q -O allxray "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/menu/allxray.sh"
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu Vmess${NC}"
+wget -q -O add-vmess "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/vmess/add-vmess.sh"
+wget -q -O del-vmess "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/vmess/del-vmess.sh"
+wget -q -O extend-vmess "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/vmess/extend-vmess.sh"
+wget -q -O trialvmess "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/vmess/trialvmess.sh"
+wget -q -O cek-vmess "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/vmess/cek-vmess.sh"
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu Vless${NC}"
+wget -q -O add-vless "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/vless/add-vless.sh"
+wget -q -O del-vless "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/vless/del-vless.sh"
+wget -q -O extend-vless "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/vless/extend-vless.sh"
+wget -q -O trialvless "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/vless/trialvless.sh"
+wget -q -O cek-vless "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/vless/cek-vless.sh"
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu Trojan${NC}"
+wget -q -O add-trojan "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/trojan/add-trojan.sh"
+wget -q -O del-trojan "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/trojan/del-trojan.sh"
+wget -q -O extend-trojan "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/trojan/extend-trojan.sh"
+wget -q -O trialtrojan "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/trojan/trialtrojan.sh"
+wget -q -O cek-trojan "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/trojan/cek-trojan.sh"
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu Shadowsocks${NC}"
+wget -q -O add-ss "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/shadowsocks/add-ss.sh"
+wget -q -O del-ss "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/shadowsocks/del-ss.sh"
+wget -q -O extend-ss "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/shadowsocks/extend-ss.sh"
+wget -q -O trialss "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/shadowsocks/trialss.sh"
+wget -q -O cek-ss "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/shadowsocks/cek-ss.sh"
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu Shadowsocks 2022${NC}"
+wget -q -O add-ss2022 "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/shadowsocks2022/add-ss2022.sh"
+wget -q -O del-ss2022 "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/shadowsocks2022/del-ss2022.sh"
+wget -q -O extend-ss2022 "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/shadowsocks2022/extend-ss2022.sh"
+wget -q -O trialss2022 "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/shadowsocks2022/trialss2022.sh"
+wget -q -O cek-ss2022 "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/shadowsocks2022/cek-ss2022.sh"
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu Socks5${NC}"
+wget -q -O add-socks "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/socks/add-socks.sh"
+wget -q -O del-socks "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/socks/del-socks.sh"
+wget -q -O extend-socks "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/socks/extend-socks.sh"
+wget -q -O trialsocks "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/socks/trialsocks.sh"
+wget -q -O cek-socks "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/socks/cek-socks.sh"
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu All Xray${NC}"
+wget -q -O add-xray "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/allxray/add-xray.sh"
+wget -q -O del-xray "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/allxray/del-xray.sh"
+wget -q -O extend-xray "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/allxray/extend-xray.sh"
+wget -q -O trialxray "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/allxray/trialxray.sh"
+wget -q -O cek-xray "https://raw.githubusercontent.com/Kulanbagong1/XRAY-MULTIPORT/main/allxray/cek-xray.sh"
+sleep 0.5
+#Prot
+wget -O port-xray "https://raw.githubusercontent.com/${GitUser}/XRAY-MULTIPORT/main/change-port/port-xray.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/${GitUser}/XRAY-MULTIPORT/main/cert.sh"
+
+#chmod
+chmod +x add-vmess
+chmod +x del-vmess
+chmod +x extend-vmess
+chmod +x trialvmess
+chmod +x cek-vmess
+chmod +x add-vless
+chmod +x del-vless
+chmod +x extend-vless
+chmod +x trialvless
+chmod +x cek-vless
+chmod +x add-trojan
+chmod +x del-trojan
+chmod +x extend-trojan
+chmod +x trialtrojan
+chmod +x cek-trojan
+chmod +x add-ss
+chmod +x del-ss
+chmod +x extend-ss
+chmod +x trialss
+chmod +x cek-ss
+chmod +x add-ss2022
+chmod +x del-ss2022
+chmod +x extend-ss2022
+chmod +x trialss2022
+chmod +x cek-ss2022
+chmod +x add-socks
+chmod +x del-socks
+chmod +x extend-socks
+chmod +x trialsocks
+chmod +x cek-socks
+chmod +x add-xray
+chmod +x del-xray
+chmod +x extend-xray
+chmod +x trialxray
+chmod +x cek-xray
+chmod +x vmess
+chmod +x vless
+chmod +x trojan
+chmod +x shadowsocks
+chmod +x shadowsocks2022
+chmod +x socks
+chmod +x allxray
+chmod +x port-xray
+chmod +x certv2ray
+
+cd
+rm -f ins-xray3.sh
+mv /root/domain /usr/local/etc/xray/domain
+cp /usr/local/etc/xray/domain /etc/xray/domain
+sleep 1
+clear;
+
+
