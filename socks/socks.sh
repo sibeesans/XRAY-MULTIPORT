7ff7f736f83d8a3c28e99a3a4184177a4eba86b7
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-05-31 10:28:23 +0700

    Update socks.sh

--- socks/socks.sh
+++ socks/socks.sh
@@ -1,13 +1,13 @@
 #wget https://github.com/${GitUser}/
 GitUser="Kulanbagong1"
-
-# // IZIN SCRIPT
-export MYIP=$(curl -sS ipv4.icanhazip.com)
-
-# // Valid Script
+#IZIN SCRIPT
+MYIP=$(curl -sS ipv4.icanhazip.com)
+echo -e "\e[32mloading...\e[0m"
+clear
+# Valid Script
 VALIDITY () {
     today=`date -d "0 days" +"%Y-%m-%d"`
-    Exp1=$(curl -sS https://raw.githubusercontent.com/${GitUser}/izinn/main/ipvps.conf | grep $MYIP | awk '{print $4}')
+    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/izinn/main/ipvps.conf | grep $MYIP | awk '{print $4}')
     if [[ $today < $Exp1 ]]; then
     echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
     else
@@ -16,50 +16,70 @@ VALIDITY () {
     exit 0
 fi
 }
-IZIN=$(curl -sS https://raw.githubusercontent.com/${GitUser}/izinn/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
+IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/izinn/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
 if [ $MYIP = $IZIN ]; then
 echo -e "\e[32mPermission Accepted...\e[0m"
 VALIDITY
-sleep 0.1
 else
 echo -e "\e[31mPermission Denied!\e[0m";
 echo -e "\e[31mPlease buy script first\e[0m"
 exit 0
 fi
+echo -e "\e[32mloading...\e[0m"
 clear
+# LINE COLOUR
+line=$(cat /etc/line)
+# TEXT COLOUR BELOW
+below=$(cat /etc/below)
+# BACKGROUND TEXT COLOUR
+back_text=$(cat /etc/back)
+# NUMBER COLOUR
+number=$(cat /etc/number)
+# TEXT ON BOX COLOUR
+box=$(cat /etc/box)
+clear
+red='\e[1;31m'
+green='\e[0;32m'
 NC='\e[0m'
-DEFBOLD='\e[39;1m'
-RB='\e[31;1m'
-GB='\e[32;1m'
-YB='\e[33;1m'
-BB='\e[34;1m'
-MB='\e[35;1m'
-CB='\e[35;1m'
-WB='\e[37;1m'
+MYIP=$(wget -qO- ifconfig.me/ip);
 clear
-echo -e "${RB}————————————————————————————————————————————————————————${NC}"
-echo -e "              ${GB}----- [ Socks5 Menu ] -----${NC}               "
-echo -e "${RB}————————————————————————————————————————————————————————${NC}"
 echo -e ""
+echo -e "   \e[$line════════════════════════════════════════\e[m"
+echo -e "   \e[$back_text             \e[30m═[\e[$box MENU Sowdowsok5\e[30m ]═          \e[m"
+echo -e "   \e[$line════════════════════════════════════════\e[m"
 echo -e " ${GB}[1]${NC} ${YB}Create Account Socks5${NC} "
 echo -e " ${GB}[2]${NC} ${YB}Trial Account Socks5${NC} "
 echo -e " ${GB}[3]${NC} ${YB}Extend Account Socks5${NC} "
 echo -e " ${GB}[4]${NC} ${YB}Delete Account Socks5${NC} "
 echo -e " ${GB}[5]${NC} ${YB}Check User Login${NC} "
+echo -e "   \e[$line════════════════════════════════════════\e[m"
+echo -e "   \e[$back_text \e[$box x)   MENU                             \e[m"
+echo -e "   \e[$line════════════════════════════════════════\e[m"
+echo -e "\e[$line"
+read -p "    Please Input Number  [1-5 or x] :  "  sys
 echo -e ""
-echo -e " ${GB}[0]${NC} ${YB}Back To Menu${NC}"
-echo -e ""
-echo -e "${RB}————————————————————————————————————————————————————————${NC}"
-echo -e ""
-read -p " Select menu :  "  opt
-echo -e ""
-case $opt in
-1) clear ; add-socks ; exit ;;
-2) clear ; trialsocks ; exit ;;
-3) clear ; extend-socks ; exit ;;
-4) clear ; del-socks ; exit ;;
-5) clear ; cek-socks ; exit ;;
-0) clear ; menu ; exit ;;
-x) exit ;;
-*) echo -e "salah tekan " ; sleep 1 ; socks ;;
+case $sys in
+1)
+add-socks
+;;
+2)
+trialsocks
+;;
+3)
+extend-socks
+;;
+4)
+del-socks
+;;
+5)
+cek-socks
+;;
+x)
+menu
+;;
+*)
+echo "Please enter an correct number"
+sleep 1
+system
+;;
 esac
