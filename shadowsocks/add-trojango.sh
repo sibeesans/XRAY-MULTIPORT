cbb520e3304e5fb392976ae60d1c3dbf52174596
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-05-31 20:50:01 +0700

    Update and rename add-ss.sh to add-trojango.sh

---
+++ shadowsocks/add-trojango.sh
@@ -0,0 +1,122 @@
+#wget https://github.com/${GitUser}/
+GitUser="Kulanbagong1"
+
+# // IZIN SCRIPT
+export MYIP=$(curl -sS ipv4.icanhazip.com)
+
+# Valid Script
+VALIDITY () {
+    clear
+    today=`date -d "0 days" +"%Y-%m-%d"`
+    Exp1=$(curl -sS https://raw.githubusercontent.com/${GitUser}/izinn/main/ipvps.conf | grep $MYIP | awk '{print $4}')
+    if [[ $today < $Exp1 ]]; then
+    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
+    else
+    echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m";
+    echo -e "\e[31mPlease renew your ipvps first\e[0m"
+    exit 0
+fi
+}
+IZIN=$(curl -sS https://raw.githubusercontent.com/${GitUser}/izinn/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo -e "\e[32mPermission Accepted...\e[0m"
+VALIDITY
+sleep 0.1
+else
+echo -e "\e[31mPermission Denied!\e[0m";
+echo -e "\e[31mPlease buy script first\e[0m"
+exit 0
+fi
+
+# // PROVIDED
+clear
+source /var/lib/premium-script/ipvps.conf
+export creditt=$(cat /root/provided)
+
+# // BANNER COLOUR
+export banner_colour=$(cat /etc/banner)
+
+# // TEXT ON BOX COLOUR
+export box=$(cat /etc/box)
+
+# // LINE COLOUR
+export line=$(cat /etc/line)
+
+# // TEXT COLOUR ON TOP
+export text=$(cat /etc/text)
+
+# // TEXT COLOUR BELOW
+export below=$(cat /etc/below)
+
+# // BACKGROUND TEXT COLOUR
+export back_text=$(cat /etc/back)
+
+# // NUMBER COLOUR
+export number=$(cat /etc/number)
+
+# // TOTAL ACC CREATE VMESS WS
+#export total1=$(grep -c -E "^#vms " "/usr/local/etc/xray/vmess.json")
+
+# // TOTAL ACC CREATE  VLESS WS
+#export total2=$(grep -c -E "^#vls " "/usr/local/etc/xray/vless.json")
+
+# // TOTAL ACC CREATE  VLESS TCP XTLS
+#export total3=$(grep -c -E "^#vxtls " "/usr/local/etc/xray/config.json")
+if [[ "$IP" = "" ]]; then
+     domain=$(cat /usr/local/etc/xray/domain)
+else
+     domain=$IP
+
+NC='\e[0m'
+DEFBOLD='\e[39;1m'
+RB='\e[31;1m'
+GB='\e[32;1m'
+YB='\e[33;1m'
+BB='\e[34;1m'
+MB='\e[35;1m'
+CB='\e[35;1m'
+WB='\e[37;1m'
+clear
+
+trgo="$(cat ~/log-install.txt | grep -w "Trojan Go" | cut -d: -f2|sed 's/ //g')"
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
+		read -rp "User : " -e user
+		user_EXISTS=$(grep -w $user /etc/trojan-go/akun.conf | wc -l)
+
+		if [[ ${user_EXISTS} == '1' ]]; then
+			echo ""
+			echo -e "Username ${RED}${user}${NC} Already On VPS Please Choose Another"
+			exit 1
+		fi
+	done
+uuid=$(cat /proc/sys/kernel/random/uuid)
+read -p "Expired (Days) : " masaaktif
+sed -i '/"'""$uuid""'"$/a\,"'""$user""'"' /etc/trojan-go/config.json
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+hariini=`date -d "0 days" +"%Y-%m-%d"`
+echo -e "### $user $exp" >> /etc/trojan-go/akun.conf
+systemctl restart trojan-go.service
+link="trojan-go://${uuid}@isi_bug_disini:${trgo}/?sni=${domain}&type=ws&host=${domain}&path=%2Ftrojango#$user"
+link1="trojan://${uuid}@isi_bug_disini:${trgo}/?sni=${domain}&type=ws&host=${domain}&path=%2Ftrojango#$user"
+clear
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "\E[0;41;36m          TROJAN GO          \E[0m" | tee -a /etc/log-create-user.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Remarks    : ${user}" | tee -a /etc/log-create-user.log
+echo -e "IP/Host    : ${MYIP}" | tee -a /etc/log-create-user.log
+echo -e "Address    : ${domain}" | tee -a /etc/log-create-user.log
+echo -e "Port       : ${trgo}" | tee -a /etc/log-create-user.log
+echo -e "Key        : ${uuid}" | tee -a /etc/log-create-user.log
+echo -e "Encryption : none" | tee -a /etc/log-create-user.log
+echo -e "Path       : /trojango" | tee -a /etc/log-create-user.log
+echo -e "Created    : $hariini" | tee -a /etc/log-create-user.log
+echo -e "Expired    : $exp" | tee -a /etc/log-create-user.log
+echo -e "========================="
+echo -e "Link TrGo  		: ${link}" | tee -a /etc/log-create-user.log
+echo -e "Link TrGo (v2rayNG)	: ${link1}" | tee -a /etc/log-create-user.log
+echo -e "========================="
+echo -e "Script Mod By ADAM SIJA"
+echo "" | tee -a /etc/log-create-user.log
+read -n 1 -s -r -p "Press any key to back on menu"
+
+menu
