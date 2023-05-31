2e74864ea1c82298ed3d6170ca53fa5fd818c48f
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-05-31 10:23:47 +0700

    Update shadowsocks2022.sh

--- shadowsocks2022/shadowsocks2022.sh
+++ shadowsocks2022/shadowsocks2022.sh
@@ -1,13 +1,13 @@
 #wget https://github.com/${GitUser}/
 GitUser="Kulanbagong1"
-
-# // IZIN SCRIPT
-export MYIP=$(curl -sS ipv4.icanhazip.com)
-
-# // Valid Script
+#IZIN SCRIPT
+MYIP=$(curl -sS ipv4.icanhazip.com)
+echo -e "\e[32mloading...\e[0m"
+clear
+# Valid Script
 VALIDITY () {
     today=`date -d "0 days" +"%Y-%m-%d"`
-    Exp1=$(curl -sS https://raw.githubusercontent.com/${GitUser}/izinn/main/ipvps.conf | grep $MYIP | awk '{print $4}')
+    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/izinn/main/ipvps.conf | grep $MYIP | awk '{print $4}')
     if [[ $today < $Exp1 ]]; then
     echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
     else
@@ -16,50 +16,70 @@ VALIDITY () {
     exit 0
 fi
 }
-IZIN=$(curl -sS https://raw.githubusercontent.com/${GitUser}/izinn/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
+IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/izinn/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
 if [ $MYIP = $IZIN ]; then
 echo -e "\e[32mPermission Accepted...\e[0m"
 VALIDITY
-sleep 0.1
 else
 echo -e "\e[31mPermission Denied!\e[0m";
 echo -e "\e[31mPlease buy script first\e[0m"
 exit 0
 fi
+echo -e "\e[32mloading...\e[0m"
 clear
+# LINE COLOUR
+line=$(cat /etc/line)
+# TEXT COLOUR BELOW
+below=$(cat /etc/below)
+# BACKGROUND TEXT COLOUR
+back_text=$(cat /etc/back)
+# NUMBER COLOUR
+number=$(cat /etc/number)
+# TEXT ON BOX COLOUR
+box=$(cat /etc/box)
+clear
+red='\e[1;31m'
+green='\e[0;32m'
 NC='\e[0m'
-DEFBOLD='\e[39;1m'
-RB='\e[31;1m'
-GB='\e[32;1m'
-YB='\e[33;1m'
-BB='\e[34;1m'
-MB='\e[35;1m'
-CB='\e[35;1m'
-WB='\e[37;1m'
+MYIP=$(wget -qO- ifconfig.me/ip);
 clear
-echo -e "${RB}————————————————————————————————————————————————————————${NC}"
-echo -e "          ${GB}----- [ Shadowsocks 2022 Menu ] -----${NC}         "
-echo -e "${RB}————————————————————————————————————————————————————————${NC}"
 echo -e ""
+echo -e "   \e[$line════════════════════════════════════════\e[m"
+echo -e "   \e[$back_text             \e[30m═[\e[$box MENU Sowdowsok\e[30m ]═          \e[m"
+echo -e "   \e[$line════════════════════════════════════════\e[m"
 echo -e " ${GB}[1]${NC} ${YB}Create Account Shadowsocks 2022${NC} "
 echo -e " ${GB}[2]${NC} ${YB}Trial Account Shadowsocks 2022${NC} "
 echo -e " ${GB}[3]${NC} ${YB}Extend Account Shadowsocks 2022${NC} "
 echo -e " ${GB}[4]${NC} ${YB}Delete Account Shadowsocks 2022${NC} "
 echo -e " ${GB}[5]${NC} ${YB}Check User Login${NC} "
+echo -e "   \e[$line════════════════════════════════════════\e[m"
+echo -e "   \e[$back_text \e[$box x)   MENU                             \e[m"
+echo -e "   \e[$line════════════════════════════════════════\e[m"
+echo -e "\e[$line"
+read -p "    Please Input Number  [1-5 or x] :  "  sys
 echo -e ""
-echo -e " ${GB}[0]${NC} ${YB}Back To Menu${NC}"
-echo -e ""
-echo -e "${RB}————————————————————————————————————————————————————————${NC}"
-echo -e ""
-read -p " Select menu :  "  opt
-echo -e ""
-case $opt in
-1) clear ; add-ss2022 ; exit ;;
-2) clear ; trialss2022 ; exit ;;
-3) clear ; extend-ss2022 ; exit ;;
-4) clear ; del-ss2022 ; exit ;;
-5) clear ; cek-ss2022 ; exit ;;
-0) clear ; menu ; exit ;;
-x) exit ;;
-*) echo -e "salah tekan " ; sleep 1 ; shadowsocks2022 ;;
+case $sys in
+1)
+add-ss2022
+;;
+2)
+trialss2022
+;;
+3)
+extend-ss2022
+;;
+4)
+del-ss2022
+;;
+5)
+cek-ss2022
+;;
+x)
+menu
+;;
+*)
+echo "Please enter an correct number"
+sleep 1
+system
+;;
 esac
